e10f8e0c6be5b67745aa4e08c92804366b8e0168
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2021-02-23 02:46:55 +0900

    add test_get_spot_prices and test_spot_prices_to_csv

--- tests/test_ec2_spot_price.py
+++ tests/test_ec2_spot_price.py
@@ -1,5 +1,18 @@
-from ec2_spot_price import __version__
+from ec2_spot_price import __version__, get_spot_prices, spot_prices_to_csv
 
 
 def test_version():
-    assert __version__ == '0.1.16'
+    assert __version__ == '0.1.17'
+
+def test_get_spot_prices():
+    r = get_spot_prices(['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'],
+                        ['c5.xlarge', 'c5d.xlarge'],
+                        ['Linux/UNIX'])
+    assert len(r) == 28
+
+def test_spot_prices_to_csv():
+    r = get_spot_prices(['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'],
+                        ['c5.xlarge', 'c5d.xlarge'],
+                        ['Linux/UNIX'])
+    csv = spot_prices_to_csv(r)
+    assert len(csv) == 2034
