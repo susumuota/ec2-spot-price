0609910305d4ef7df1cc3aecb46501313a02aa40
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2021-02-26 02:19:36 +0900

    bump version

--- src/ec2_spot_price/__init__.py
+++ src/ec2_spot_price/__init__.py
@@ -1,5 +1,5 @@
 from .ec2_spot_price import get_spot_prices, print_csv, print_table
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 __all__ = ['get_spot_prices', 'print_csv', 'print_table']
--- tests/test_ec2_spot_price.py
+++ tests/test_ec2_spot_price.py
@@ -2,7 +2,7 @@ from ec2_spot_price import __version__, get_spot_prices, print_csv, print_table
 
 
 def test_version():
-    assert __version__ == '0.2.0'
+    assert __version__ == '0.2.1'
 
 def test_get_spot_prices():
     sp = get_spot_prices(['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'],
@@ -16,7 +16,7 @@ def test_print_csv(capsys):
                         ['Linux/UNIX'])
     print_csv(sp)
     captured = capsys.readouterr()
-    assert len(captured.out) == 1960
+    assert len(captured.out) == 1791
 
 def test_print_table(capsys):
     sp = get_spot_prices(['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'],
